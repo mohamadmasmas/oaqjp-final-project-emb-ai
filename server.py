6c312f9ec60ad328b818e2db50611996df0fee66
mohamadmasmas <masmasmohamad@gmail.com>
2024-08-05 10:34:17 -0400

    nothing

--- server.py
+++ server.py
@@ -1,18 +1,20 @@
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
+app = Flask("Emotion Detection")
+
 @app.route("/emotionDetector")
 def sent_detector():
   
     text_to_analyze = request.args.get('textToAnalyze')
 
     response = emotion_detector(text_to_analyze)
-    
-    formatted_text = "For the given statement, the system response is" + str(response)[1:len(str(response))-1]+ ". The dominant emotion is "+response["dominant_emotion"]
-    print("For the given statement, the system response is" + str(response)[1:len(str(response))-1]+ ". The dominant emotion is "+response["dominant_emotion"])
+    dominant_emotion = response.pop("dominant_emotion")
+    response_text = str(response)[1:len(str(response))-1]
 
-    # Return a formatted string with the sentiment label and score
+    formatted_text = "For the given statement, the system response is " + response_text + ". The dominant emotion is "+dominant_emotion
+
     return formatted_text
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=4000)
+    app.run(host="0.0.0.0", port=5000)
